d82080d998279334e93c69f0e6c6df9b526734e1
Diksha65 <agarwaldiksha6596@gmail.com>
2019-08-12 00:51:28 +0530

    Implementing Pagination

--- app/src/main/java/com/example/dunzoassignment/ListActivity.kt
+++ app/src/main/java/com/example/dunzoassignment/ListActivity.kt
@@ -11,17 +11,25 @@ import com.squareup.okhttp.Request
 import com.squareup.okhttp.Response
 import kotlinx.android.synthetic.main.activity_list.*
 import java.io.IOException
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 
 class ListActivity : AppCompatActivity() {
 
     private val searchTag = "Search"
-    lateinit var searchText : String
+    private var searchText : String?= null
+
+    //Atomic values are used since many threads are calling it.
+    private var startIndex = AtomicInteger(1)
+    private var requestSent = AtomicBoolean(false)
+    private var hasMoreImages = AtomicBoolean(true)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
         searchText = intent.getStringExtra(searchTag)!!
+        googleSearch(searchText!!)
 
         recyclerview.apply {
             layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
@@ -30,19 +38,56 @@ class ListActivity : AppCompatActivity() {
             //removes the blink on the image on clicking the view holder
             (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
 
-            adapter = ImageAdapter().apply {
-                addDetailButtonCallback { finalObject ->
-                    val intent = Intent(this@ListActivity, DetailActivity::class.java)
+            adapter = createAdapter()
+        }
+    }
 
-                    val bundle = Bundle().apply { putSerializable("ImageObject", finalObject) }
-                    intent.putExtras(bundle)
+    fun attachOnScrollListener(recyclerView: RecyclerView) {
 
-                    startActivity(intent)
+        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val totalItems = layoutManager.itemCount
+                val visibleItems = layoutManager.childCount
+                val firstVisible = layoutManager.findFirstVisibleItemPosition()
+
+                if(totalItems != 0 &&
+                        visibleItems != 0 &&
+                        firstVisible + visibleItems >= totalItems) {
+
+                    logDebug("Reached bottom of Recycler view")
+                    if(!requestSent.getAndSet(true) && hasMoreImages.get()) {
+                        googleSearch(searchText!!, startIndex.toString())
+                    } else {
+                        notifyUser(
+                            if(hasMoreImages.get()) "Ongoing Request" else "No more images from the API"
+                        )
+                    }
+                } else {
+                    logDebug("Total or visible items 0")
                 }
             }
+        })
+    }
+
+    fun createAdapter(): ImageAdapter {
+        return ImageAdapter().apply {
+
+            addDetailButtonCallback { finalObject ->
+                val intent = Intent(this@ListActivity, DetailActivity::class.java)
+
+                val bundle = Bundle().apply { putSerializable("ImageObject", finalObject) }
+                intent.putExtras(bundle)
+
+                startActivity(intent)
+            }
+        }
+    }
 
 
-            fun googleSearch(
+    fun googleSearch(
                 context: Context,
                 searchString: String,
                 startIndex: String
